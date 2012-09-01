c07c28f3ee2584bceca8a75b21ef34ade3dac663
costello <costelljohn@gmail.com>
2012-09-01 19:35:01 +0100

    web interface for following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,13 +8,13 @@ describe "Authentication" do
       describe "in the Users controller" do
 
         describe "visiting the following page" do
-          before { visit following_user_path(user) }
-          it { should have_selector('title', text: 'Sign in') }
+          before { visit following_user_path(user) }          
+          it { page.should have_selector('h2', text: 'Sign in') }
         end
         
         describe "visiting the followers page" do
-          before { visit followers_user_path(user) }
-          it { should have_selector('title', text: 'Sign in') }
+          before { visit followers_user_path(user) }          
+          it { page.should have_selector('h2', text: 'Sign in') }
         end
       end
       
@@ -32,11 +32,11 @@ describe "Authentication" do
       describe "in the Relationships controller" do
         describe "submitting to the create action" do
           before { post relationships_path }
-          specify { response.should redirect_to(signin_path) }
+          specify { response.should redirect_to(new_user_session_path) }
         end
         describe "submitting to the destroy action" do
           before { delete relationship_path(1) }
-          specify { response.should redirect_to(signin_path) }
+          specify { response.should redirect_to(new_user_session_path) }
         end
       end
     end
