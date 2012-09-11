de7de3d716ace892251390ae0a7784578f9680df
costello <costelljohn@gmail.com>
2012-09-11 14:28:38 +0100

    working tposts pposts paginated multiple models

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,9 +5,9 @@ class StaticPagesController < ApplicationController
     
     if user_signed_in?
       @project = current_user.projects.build 
-      @project_feed_items = current_user.project_feed.paginate(page: params[:page])
-      @topic_feed_items = current_user.topic_feed.paginate(page: params[:page])
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @project_feed_items = current_user.project_feed.paginate(page: params[:project_page])
+      @topic_feed_items = current_user.topic_feed.paginate(page: params[:topic_page])
+      @feed_items = current_user.feed.paginate(page: params[:feed_page])
     end
   end
 
