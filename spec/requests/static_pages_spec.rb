a991aed2d0f1e9891853fedac133720a45dcb1a5
John Costello <costelljohn@gmail.com>
2012-09-13 15:49:36 +0100

    added new create topic project links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -21,13 +21,29 @@ describe "StaticPages" do
     end
   end
   
+  describe "for non signed-in users" do
+    let(:user) { FactoryGirl.create(:user) } 
+    before do    
+      visit root_path
+    end
+    subject{page}
+
+    it { should_not have_link(user.name, href: user_path(user)) }
+    it { should_not have_link("new topic", href: new_user_topic_path(user)) }
+    it { should_not have_link("new project", href: new_user_project_path(user)) }
+  end
+  
   describe "for signed-in users" do
     let(:user) { create_logged_in_user } 
     before do
       FactoryGirl.create(:post, user: user, postable: (FactoryGirl.create(:ppost, title: "Foo", summary: "football"))) 
       visit root_path
     end
-    
+    subject{page}
+
+    it { should have_link(user.name, href: user_path(user)) }
+    it { should have_link("new topic", href: new_user_topic_path(user)) }
+    it { should have_link("new project", href: new_user_project_path(user)) }
     it "should render the user's post feed" do
       user.feed.each do |item|
         page.should have_selector("li##{item.id}", text: item.postable.title)
