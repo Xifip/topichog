bbf2fd92e48af0e0e2553700c2d6e5c6fa7e63f2
John Costello <costelljohn@gmail.com>
2012-09-25 09:42:36 +0100

    migrations editing

--- db/migrate/20120912075835_remove_feeable_from_projects.rb
+++ db/migrate/20120912075835_remove_feeable_from_projects.rb
@@ -1,7 +1,7 @@
 class RemoveFeeableFromProjects < ActiveRecord::Migration
-  def change
-    remove_column :projects, :feedable_id, :integer
-    remove_column :projects, :feedable_type, :string
-  end
+  #def change
+  #  remove_column :projects, :feedable_id, :integer
+  #  remove_column :projects, :feedable_type, :string
+  #end
 
 end
--- db/migrate/20120912075940_remove_feeable_from_topics.rb
+++ db/migrate/20120912075940_remove_feeable_from_topics.rb
@@ -1,6 +1,6 @@
 class RemoveFeeableFromTopics < ActiveRecord::Migration
-  def change
-    remove_column :topics, :feedable_id, :integer
-    remove_column :topics, :feedable_type, :string
-  end
+  #def change
+  #  remove_column :topics, :feedable_id, :integer
+  #  remove_column :topics, :feedable_type, :string
+  #end
 end
