a57c1b99932f87fb4a1514d3d00f2e244e134c7b
John Costello <costelljohn@gmail.com>
2012-10-18 08:44:40 +0100

    try EU bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     provider: "AWS",
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    #region: 'eu-west-1'
+    region: 'eu-west-1'
   }
-  config.fog_directory = "topichogus"  
+  config.fog_directory = "topichogire"  
   
   #config.max_file_size     = 700.kilobytes
   #config.fog_directory = ENV["AWS_S3_BUCKET"]
