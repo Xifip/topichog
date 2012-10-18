d069b2671a7457d5036190aee81f42265e27a2f1
John Costello <costelljohn@gmail.com>
2012-10-18 08:38:07 +0100

    try US bucket

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 TopicHog::Application.initialize!
 
-#AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
+
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,11 @@ CarrierWave.configure do |config|
     provider: "AWS",
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-   :region                 => 'eu-west-1'
+    #region: 'eu-west-1'
   }
-  config.fog_directory = "topichogire"
+  config.fog_directory = "topichogus"  
+  
   #config.max_file_size     = 700.kilobytes
   #config.fog_directory = ENV["AWS_S3_BUCKET"]
-  config.fog_host = 'https://s3-eu-west-1.amazonaws.com/topichogire'
+  #config.fog_host = 'https://s3-eu-west-1.amazonaws.com/topichogire'
 end
